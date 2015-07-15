a2e38c1c0a3b4cf29b4618222b7f606a083e394a
Georg G <nilsding@nilsding.org>
2015-07-15 10:05:40 +0200

    NetworkSettingsWidget now loads/displays/store the config

--- src/settings/networksettingswidget.cpp
+++ src/settings/networksettingswidget.cpp
@@ -3,12 +3,53 @@
 
 NetworkSettingsWidget::NetworkSettingsWidget(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::NetworkSettingsWidget)
+    _ui(new Ui::NetworkSettingsWidget)
 {
-    ui->setupUi(this);
+    _ui->setupUi(this);
+    loadSettings();
+    _ui->qcbNetwork->clear();
+    for (auto network : _networks) {
+        _ui->qcbNetwork->addItem(network->name());
+    }
+
+    int i = 0;
+    for (; i < _networks.size() && _networks.at(i)->isActive(); i++);
+    _ui->qcbNetwork->setCurrentIndex(i);
 }
 
 NetworkSettingsWidget::~NetworkSettingsWidget()
 {
-    delete ui;
+    delete _ui;
+}
+
+void NetworkSettingsWidget::loadSettings()
+{
+    int size = _SETTINGS.beginReadArray("Networks");
+        for (int i = 0; i < size; i++) {
+            _SETTINGS.setArrayIndex(i);
+            IrcNetwork *network = new IrcNetwork;
+            network->setName(_SETTINGS.value("name").toString());
+            network->setServers(_SETTINGS.value("servers").toStringList());
+            network->setActive(_SETTINGS.value("active").toBool());
+            _networks.append(network);
+        }
+    _SETTINGS.endArray();
+}
+
+void NetworkSettingsWidget::storeSettings()
+{
+    _SETTINGS.beginWriteArray("Networks");
+        for (int i = 0; i < _networks.size(); ++i) {
+            _SETTINGS.setArrayIndex(i);
+            _SETTINGS.setValue("name", _networks.at(i)->name());
+            _SETTINGS.setValue("servers", *_networks.at(i)->servers());
+            _SETTINGS.setValue("active", _networks.at(i)->isActive());
+        }
+    _SETTINGS.endArray();
+}
+
+void NetworkSettingsWidget::on_qcbNetwork_currentIndexChanged(int index)
+{
+    if (index == -1) return;
+    _ui->qlvServers->setModel(new QStringListModel(*_networks.at(index)->servers()));
 }
--- src/settings/networksettingswidget.h
+++ src/settings/networksettingswidget.h
@@ -2,6 +2,9 @@
 #define NETWORKSETTINGSWIDGET_H
 
 #include <QWidget>
+#include <QStringListModel>
+#include "nsettings.h"
+#include "../core/ircnetwork.h"
 
 namespace Ui {
 class NetworkSettingsWidget;
@@ -15,8 +18,16 @@ public:
     explicit NetworkSettingsWidget(QWidget *parent = 0);
     ~NetworkSettingsWidget();
 
+public slots:
+    void loadSettings();
+    void storeSettings();
+
+private slots:
+    void on_qcbNetwork_currentIndexChanged(int index);
+
 private:
-    Ui::NetworkSettingsWidget *ui;
+    Ui::NetworkSettingsWidget *_ui;
+    QList<IrcNetwork *> _networks;
 };
 
 #endif // NETWORKSETTINGSWIDGET_H
