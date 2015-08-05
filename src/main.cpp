27184523f762ac9637d327dc94b58e59137390b3
Georg G <nilsding@nilsding.org>
2015-08-05 11:51:19 +0200

    actually set up translations

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,26 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QTranslator>
+#include <QLocale>
+#include <QLibraryInfo>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    // seed qrand()
     qsrand((uint) QTime::currentTime().msec());
 
+    // set up translations
+    QTranslator qtTranslator;
+    qtTranslator.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    a.installTranslator(&qtTranslator);
+
+    QTranslator myappTranslator;
+    myappTranslator.load("nIRC_" + QLocale::system().name());
+    a.installTranslator(&myappTranslator);
+
+    // initialize and show main window
     MainWindow w;
     w.show();
 
