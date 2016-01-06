d83ec6cf530946028b2a1952924a72fc0b3806d4
Georg G <nilsding@nilsding.org>
2016-01-06 16:14:06 +0100

    added {,re,un}loadScripts methods

--- src/script/njsengine.cpp
+++ src/script/njsengine.cpp
@@ -1,7 +1,16 @@
 #include "njsengine.h"
 #include <QDebug>
 
+
+QJSEngine *NJSEngine::_engine = new QJSEngine();
 bool NJSEngine::_initialized = false;
+QList<NScript *> *NJSEngine::_scripts = new QList<NScript *>();
+NScriptBindings *NJSEngine::_bindings = new NScriptBindings();
+
+NJSEngine::NJSEngine(QObject *parent) : QObject(parent)
+{
+
+}
 
 void NJSEngine::init()
 {
@@ -10,7 +19,49 @@ void NJSEngine::init()
     }
     qDebug() << "Initialized NJSEngine";
 
-    // TODO: add some globals to the engine
+    QJSValue scriptObj = _engine->newQObject(_bindings);
+    _engine->globalObject().setProperty("IrrerrC", scriptObj);
 
     _initialized = true;
+}
+
+void NJSEngine::loadScripts()
+{
+    QString scriptDir = QFileInfo(_SETTINGS.fileName()).absolutePath() + "/scripts";
+    QDir d(scriptDir);
+    if (!d.exists()) {
+        d.mkpath(scriptDir);
+    }
+    QStringList filters;
+    filters << "*.js";
+    QStringList entries = d.entryList(filters, QDir::NoDotAndDotDot | QDir::Files);
+
+    for (const QString& entry : entries) {
+        NScript *script = new NScript(scriptDir + "/" + entry);
+        _scripts->append(script);
+        script->load(_engine);
+    }
+}
+
+void NJSEngine::reloadScripts()
+{
+    QTimer::singleShot(0, this, [&]() {
+        qDebug() << "Reloading scripts...";
+        unloadScripts();
+        _scripts->clear();
+        _engine->collectGarbage();
+        _engine->deleteLater();
+        _initialized = false;
+        _engine = new QJSEngine;
+        NJSEngine::init();
+        loadScripts();
+    });
+}
+
+void NJSEngine::unloadScripts()
+{
+    for (NScript *script : *_scripts) {
+        script->unload(_engine);
+        script->deleteLater();
+    }
 }
--- src/script/njsengine.h
+++ src/script/njsengine.h
@@ -3,6 +3,12 @@
 
 #include <QObject>
 #include <QJSEngine>
+#include <QDir>
+#include <QFileInfo>
+#include <QTimer>
+#include "nscript.h"
+#include "nscriptbindings.h"
+#include "settings/nsettings.h"
 
 #define _JSENGINE NJSEngine::instance()
 
@@ -10,20 +16,28 @@ class NJSEngine : public QObject
 {
     Q_OBJECT
 public:
+    explicit NJSEngine(QObject *parent);
+
     static void init();
 
     static QJSEngine& instance()
     {
-        return _engine;
+        return *_engine;
     }
 
 signals:
 
 public slots:
+    void loadScripts();
+    void reloadScripts();
+    void unloadScripts();
 
 private:
-    static QJSEngine _engine;
+    static QJSEngine *_engine;
     static bool _initialized;
+    static QList<NScript *> *_scripts;
+    static NScriptBindings *_bindings;
+
     Q_DISABLE_COPY(NJSEngine)
 };
 
